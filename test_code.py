a6f8190e195cdcd13c0429adf6a131a2b97d1c0c
Francois <myemail.com>
2020-05-10 18:22:37 +0200

    Revert "Added some changes"
    I need my changes on the remote repo
    
    This reverts commit bdfbe57f5c523ce78ce8bfbd01e2a09ab82b3019.

--- test_code.py
+++ test_code.py
@@ -2,9 +2,9 @@ def max_num(x, y):
     max = x if x > y else y
     return max
 
-def print_name(name):
-    print(name)
+def print_name_age(name, age):
+    print(name, 'is', age, 'old')
 
 print(max_num(24,89))
 
-print('Francis is the man')
+print_name_age('Francis', 42)
