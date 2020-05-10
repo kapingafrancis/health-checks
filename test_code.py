bdfbe57f5c523ce78ce8bfbd01e2a09ab82b3019
Francois <myemail.com>
2020-05-10 18:15:49 +0200

    Added some changes

--- test_code.py
+++ test_code.py
@@ -2,9 +2,9 @@ def max_num(x, y):
     max = x if x > y else y
     return max
 
-def print_name_age(name, age):
-    print(name, 'is', age, 'old')
+def print_name(name):
+    print(name)
 
 print(max_num(24,89))
 
-print_name_age('Francis', 42)
+print('Francis is the man')
